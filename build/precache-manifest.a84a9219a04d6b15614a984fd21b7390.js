ad93f6e1067c676b5731667800f10253ba82161e
Petr Patek <petrpatekml@gmail.com>
2019-12-30 17:23:34 +0100

    removed service worker.

---
+++ build/precache-manifest.a84a9219a04d6b15614a984fd21b7390.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "118a75b5f6c6cbf766ec84e98b13dc4b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32d1357a59e7dccf9db7",
+    "url": "/static/css/2.7ececfe9.chunk.css"
+  },
+  {
+    "revision": "40aa394a71c31f3a76e9",
+    "url": "/static/css/main.db2878cf.chunk.css"
+  },
+  {
+    "revision": "32d1357a59e7dccf9db7",
+    "url": "/static/js/2.761ce214.chunk.js"
+  },
+  {
+    "revision": "40aa394a71c31f3a76e9",
+    "url": "/static/js/main.b2e85b4c.chunk.js"
+  },
+  {
+    "revision": "d5cc954c1aa1da9418ae",
+    "url": "/static/js/runtime-main.d67d3e89.js"
+  }
+]);
