5c99d7f06bb06fe0ab468e7ddec306aa53ddcb3b
František Nesveda <frantisek@apify.com>
2022-10-22 19:16:12 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/input-schema-editor-react/precache-manifest.ee614522c95295964fb089d82b7d0e7e.js"
+  "/input-schema-editor-react/precache-manifest.3872ae8c2767391202eccbc4212d23e2.js"
 );
 
 self.addEventListener('message', (event) => {
