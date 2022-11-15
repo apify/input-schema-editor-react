12b0ac0b0b07cd68c01b80cd2c24061c110107d7
František Nesveda <frantisek@apify.com>
2022-11-15 13:34:46 +0100

    Updates

--- precache-manifest.8ce1eeadf5794106d34483a74674ba53.js
+++ precache-manifest.8ce1eeadf5794106d34483a74674ba53.js
@@ -1,6 +1,6 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "06261f8f267792dc71bc22810622ffc6",
+    "revision": "7e1e1a23dd613b4d91b5a58136b0897b",
     "url": "/input-schema-editor-react/index.html"
   },
   {
@@ -8,7 +8,7 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/input-schema-editor-react/static/css/2.7ececfe9.chunk.css"
   },
   {
-    "revision": "dc519d41112d0db0927d",
+    "revision": "712164428f4449096749",
     "url": "/input-schema-editor-react/static/css/main.db2878cf.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/input-schema-editor-react/static/js/2.8c1e78c9.chunk.js"
   },
   {
-    "revision": "dc519d41112d0db0927d",
-    "url": "/input-schema-editor-react/static/js/main.2bd7cbc1.chunk.js"
+    "revision": "712164428f4449096749",
+    "url": "/input-schema-editor-react/static/js/main.636dca9f.chunk.js"
   },
   {
     "revision": "e360fdcfa832d4006594",
